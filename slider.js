02b1ef5a12021f17f30b1944ea4361af64e32c33
nabi_koban <52134694+ngadimov1999@users.noreply.github.com>
2021-02-21 20:19:54 +0500

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,84 @@
+var label = $("label[for=r1],label[for=r2]");
+
+function change() {
+    $(label).css('background', '#264160');
+    $(this).css('background', 'none');
+}
+
+var isLeft = false;
+var timeOut = 5000;
+
+setTimeout(autoSlide(), timeOut);
+
+var coin = 0;
+
+$(label[0]).css('background', 'none');
+
+$(label[1]).click( function(){
+    $(this).prop("disable", true);
+    right();
+})
+
+function autoSlide() {
+    $(label).css('background', '#264160');
+    if (coin == 1) {
+        if (!isLeft) {
+            right();
+            $(label[1]).css('background', 'none');
+        } else {
+            left();
+            $(label[0]).css('background', 'none');
+        }
+    }
+    setTimeout(autoSlide, timeOut);
+}
+
+$(label).click(change);
+
+
+$(label[0]).click( function(){
+    $(this).prop("disable", true);
+    left();
+})
+//Движение слайдов
+coin = 1;
+
+
+
+
+var slider1 = $('#slider1');
+var slider2 = $('#slider2');
+var block = $('.block');
+
+function left() {
+    slide(slider2, slider1);
+    isLeft = false;
+}
+
+function right() {
+    slide(slider1, slider2);
+    isLeft = true;
+}
+
+function slide(slider1, slider2) {
+    slider2.css('left', '0%');
+    slider1.css('left', '-50%');
+    block.css('z-index', '6');
+    setTimeout(function () {
+        slider1.css('transition', '0s');
+        slider1.css('opacity', '0');
+        setTimeout(function () {
+            slider1.css('left', '50%');
+            setTimeout(function () {
+                slider1.css('opacity', '1');
+                slider1.css('transition', '1s');
+                block.css('z-index', '4');
+            }, 20);
+        }, 10);
+    }, 600);
+}
+
+
+
+
+
